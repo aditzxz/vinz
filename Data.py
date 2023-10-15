dfd5765d2d2009d7706a993f3d0a626c11d25bd0
aditzxz <96695404+aditzxz@users.noreply.github.com>
2023-10-15 22:58:57 +0700

    Add files via upload

---
+++ Data.py
@@ -0,0 +1,39 @@
+# Credits: @mrismanaziz
+# FROM File-Sharing-Man <https://github.com/mrismanaziz/File-Sharing-Man/>
+# t.me/SharingUserbot & t.me/Lunatic0de
+
+from pyrogram.types import InlineKeyboardButton
+
+class Data:
+    HELP = """
+
+
+
+"""
+
+    close = [
+        [InlineKeyboardButton("ᴛᴜᴛᴜᴘ", callback_data="close")]
+    ]
+
+    mbuttons = [
+        [
+            InlineKeyboardButton("ᴛᴜᴛᴜᴘ", callback_data="close")
+        ],
+    ]
+
+    buttons = [
+        [
+
+            InlineKeyboardButton("ᴛᴜᴛᴜᴘ", callback_data="close")
+        ],
+    ]
+
+    ABOUT = """
+<b>Tentang Bot ini:
+
+@{} adalah Bot Telegram untuk menyimpan Postingan atau File yang dapat Diakses melalui Link Khusus.
+
+ • Creator: @{}
+
+
+"""
